1580f3c964de99bdf862034b4ecf79707def129b
LicOlaso <112059793+LicOlaso@users.noreply.github.com>
2022-08-25 03:49:15 -0300

    Configurable database connection
    It allows connecting to a database configuring the basic parameters (server, user, password and database)

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+$hostname = '127.0.0.1';
+$username = 'root';
+$password = '';
+$base_de_datos = 'prueba';
+$conn=mysqli_connect($hostname,$username,$password,$base_de_datos);
+
+echo "<pre>";
+//print_r($conn);
